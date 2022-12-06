7db7b2925ea7f942a3a79b90453e8f1958e93dd8
IliaSyrytskii <polonamocyi2525@gmail.com>
2022-12-06 15:41:05 +0300

    commit #2

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,51 +1,40 @@
 import java.util.Scanner;
 
-
-
 public class CreditCard {
-    private Scanner sc = new Scanner(System.in);
-    private int money;
-
-    class Description {
+    Scanner sc = new Scanner(System.in);
+    int money;
 
         int accountNumber;
         int balance;
 
 
-        public  Description(int accountNumber, int balance) {
+        public CreditCard(int accountNumber, int balance) {
             this.accountNumber = accountNumber;
             this.balance = balance;
         }
 
         public void plus() {
-            balance += money;
-        }
-
-        public void minus() {
-            balance -= money;
-        }
-
-        public void condition() {
-            System.out.println();
-        }
-    }
-        public void main(String[] args) {
-        Description alfa = new Description(1060, 1500);
-        Description belarus = new Description(990, 1300);
-        Description prior = new Description(1100, 600);
-            System.out.println("Enter card");
-
-
-            //добавляем
             System.out.println("Enter summ of accrual");
             money = sc.nextInt();
-            alfa.plus();
-            //изъятие
+            this.balance += money;
+        }
+        public void minus() {
             System.out.println("Enter summ of removal");
             money = sc.nextInt();
-            alfa.minus();
-            //состояние
-            alfa.condition();
+            this.balance -= money;
+        }
+        public void condition() {
+            System.out.println("Account:  " + this.accountNumber + " Balance:  " + this.balance);
+        }
+
+        public static void main(String[] args) {
+            CreditCard alfa = new CreditCard(1060, 1500);
+            CreditCard belarus = new CreditCard(990, 1300);
+            CreditCard prior = new CreditCard(1100, 600);
+
+            alfa.plus(); prior.plus(); belarus.minus();
+            alfa.condition(); belarus.condition(); prior.condition();
+
 
 
 
